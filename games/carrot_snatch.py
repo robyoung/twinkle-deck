c7a3cbe0d9dc9676a045a5003e4949eba6c9e0e5
Rob Young <rob@robyoung.digital>
2023-12-13 09:49:26 +0000

    Add description to carrot snatch

--- games/carrot_snatch.py
+++ games/carrot_snatch.py
@@ -1,3 +1,14 @@
+"""Carrot snatch
+
+You play as Rudolf the Red Nosed Reindeer. Santa has a box of juicy carrots but he has
+said that they cannot be eaten until after the presents have been delivered.
+You cannot wait!
+
+Santa is at the far end of the string. He is a red light and a white light.
+In front of Santa are 3 orange carrots.
+You are Rudolf, a red dot at the other end of the string.
+You must slowly up to the carrots and take one back to the start.
+"""
 import time
 
 import twinkledeck.hal as td
@@ -6,6 +17,7 @@ from twinkledeck.pulse import RegularPulser, ErraticPulser
 SANTA_STATE_ALERT = 0
 SANTA_STATE_AWAKE = 1
 SANTA_STATE_SLEEP = 2
+
 
 class Santa:
     def __init__(self):
@@ -23,13 +35,12 @@ class Santa:
         self.state = SANTA_STATE_SLEEP
         self.pulser = RegularPulser(min_value=0.1, max_value=0.6, duration=3000)
 
-
     def show(self):
         value = self.pulser.value()
-        td.lights.set_hsv(47, 0, 1, value) # santa
-        td.lights.set_hsv(48, 0, 1, value) # santa
-        td.lights.set_hsv(49, 0, 0, 0.5) # hat
-        
+        td.lights.set_hsv(47, 0, 1, value)  # santa
+        td.lights.set_hsv(48, 0, 1, value)  # santa
+        td.lights.set_hsv(49, 0, 0, 0.5)  # hat
+
 
 class Carrot:
     def __init__(self, position):
@@ -51,9 +62,9 @@ class Rudolf:
         position = int(self.position * (td.NUM_LEDS - 3))
         pass
 
+
 def main():
     while True:
-
         time.sleep(1.0 / 60)
 
 
