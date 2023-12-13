6c94c5941739dd53c5da8a02eb761445b0ec916e
Rob Young <rob@robyoung.digital>
2023-12-13 09:44:12 +0000

    Make the tester example more useful for finding colours

--- examples/tester.py
+++ examples/tester.py
@@ -7,20 +7,29 @@ The buttons control which lights are on.
 import twinkledeck.hal as td
 import time
 
-ALL_ON = 1 << 2
+ALL_ON = 1 << 0
 TWOS_OFF = 1 << 1
-THREES_OFF = 1 << 0
+
+RGB = 0 << 2
+HSV = 1 << 2
 
 light_flags = ALL_ON
 prev_light_flags = None
-prev_hue = None
-prev_sat = None
-prev_val = None
+prev_dial1 = None
+prev_dial2 = None
+prev_dial3 = None
+
+
+def get_colour(light_flags, dial1, dial2, dial3):
+    if light_flags & HSV:
+        return dial1, dial2, dial3
+    else:
+        return int(dial1 * 255), int(dial2 * 255), int(dial3 * 255)
 
 while True:
-    hue = round(td.dial1.value, 2)
-    sat = round(td.dial2.value, 2)
-    val = round(td.dial3.value, 2)
+    dial1 = round(td.dial1.value, 2)
+    dial2 = round(td.dial2.value, 2)
+    dial3 = round(td.dial3.value, 2)
 
     if td.button1.read():
         light_flags = ALL_ON
@@ -28,31 +37,36 @@ while True:
         light_flags |= TWOS_OFF
         light_flags ^= ALL_ON
     elif td.button3.read():
-        light_flags |= THREES_OFF
-        light_flags ^= ALL_ON
+        light_flags ^= HSV  # toggle between RGB and HSV
 
     if (
         light_flags != prev_light_flags
-        or hue != prev_hue
-        or sat != prev_sat
-        or val != prev_val
+        or dial1 != prev_dial1
+        or dial2 != prev_dial2
+        or dial3 != prev_dial3
     ):
+        colour = get_colour(light_flags, dial1, dial2, dial3)
+        if light_flags & HSV:
+            print(f"H({dial1}) S({dial2}) V({dial3})")
+        else:
+            print(f"R({colour[0]}) G({colour[1]}) B({colour[2]})")
         for i in range(td.NUM_LEDS):
             if i % 2 == 0 and light_flags & TWOS_OFF:
-                should_show = False
-            elif i % 3 == 0 and light_flags & THREES_OFF:
                 should_show = False
             else:
                 should_show = True
 
             if should_show:
-                td.lights.set_hsv(i, hue, sat, val)
+                if light_flags & HSV:
+                    td.lights.set_hsv(i, *colour)
+                else:
+                    td.lights.set_rgb(i, *colour)
             else:
                 td.lights.set_hsv(i, 0, 0, 0)
 
     prev_light_flags = light_flags
-    prev_hue = hue
-    prev_sat = sat
-    prev_val = val
+    prev_dial1 = dial1
+    prev_dial2 = dial2
+    prev_dial3 = dial3
 
     time.sleep(1.0 / 60)
